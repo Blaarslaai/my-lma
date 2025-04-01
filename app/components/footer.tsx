230c256b6d799a6283bcc4a03c4f4daeb8b508d5
Valdez Pretorius <valdez.pretorius@gmail.com>
2025-04-01 10:36:50 +0200

    Created default site layout with a cookie banner and theme controller.

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,3 +1,47 @@
-export default function Navbar() {
-  return <>Footer</>;
+import ThemeToggle from "./themeToggle";
+
+export default function Footer() {
+  return (
+    <footer className="py-8 mt-8 border">
+      <div className="container mx-auto px-6 text-center">
+        <div className="mb-4">
+          <h1 className="text-xl font-semibold">
+            Some of My Links (More found on GitHub)
+          </h1>
+          <ThemeToggle />
+        </div>
+
+        {/* Social Media Links */}
+        <div className="mb-4">
+          <ul className="flex justify-center space-x-6">
+            <li>
+              <a
+                href="https://www.linkedin.com/in/roelf-pretorius-467551190/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="hover:text-gray-400"
+              >
+                LinkedIn
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://github.com/Blaarslaai"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="hover:text-gray-400"
+              >
+                GitHub
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        {/* Copyright Section */}
+        <div className="text-sm text-gray-400">
+          <p>&copy; {new Date().getFullYear()} MY-LMA. All Rights Reserved.</p>
+        </div>
+      </div>
+    </footer>
+  );
 }
