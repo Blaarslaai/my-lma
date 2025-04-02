d8e57faa0eebdc53499e2a5f9488228355528004
Valdez Pretorius <valdez.pretorius@gmail.com>
2025-04-02 10:03:15 +0200

    Fixed cookie route.

--- app/api/check-cookie/route.js
+++ app/api/check-cookie/route.js
@@ -1,8 +1,8 @@
 import { cookies } from "next/headers";
 
 export async function GET() {
-  const cookieStore = cookies();
-  const hasConsent = await cookieStore.get("cookieConsent");
+  const cookieStore = await cookies();
+  const hasConsent = cookieStore.get("cookieConsent");
 
   return Response.json({ hasConsent: !!hasConsent });
 }
